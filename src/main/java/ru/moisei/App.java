61e1be2b866f023fe4869208966fc1da45da8e0e
Andrei M <moisei555@yahoo.com>
2024-08-01 15:39:39 +0500

    git init

--- src/main/java/ru/moisei/App.java
+++ src/main/java/ru/moisei/App.java
@@ -14,7 +14,8 @@ public class App {
         System.out.println(Arrays.toString(refund(2,40)));
         searchMinMax();
     }
-
+    //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1,
+    //0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
     public static void onezero() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         int[] arr2 = arr;
@@ -33,7 +34,8 @@ public class App {
         System.out.println(Arrays.toString(arr2));
 
     }
-
+    //Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его
+    //значениями 1 2 3 4 5 6 7 8 … 100;
     public static void massOrder() {
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
@@ -41,7 +43,8 @@ public class App {
         }
         System.out.println(Arrays.toString(arr));
     }
-
+    //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6
+    //умножить на 2;
     public static void multiplyNumbers() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
@@ -51,7 +54,11 @@ public class App {
         }
         System.out.println(Arrays.toString(arr));
     }
-
+    //Создать квадратный двумерный целочисленный массив (количество строк и столбцов
+    //одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+    //(можно только одну из диагоналей, если обе сложно). Определить элементы одной из
+    //диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0],
+    //[1][1], [2][2], …, [n][n];
     public static void diagMass() {
         int[][] arr = new int[9][9];
         for (int i = 0; i < arr.length; i++) {
@@ -64,7 +71,8 @@ public class App {
             System.out.println();
         }
     }
-
+    //Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий
+    //одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
     public static int[] refund(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < arr.length; i++) {
@@ -73,6 +81,7 @@ public class App {
         }
         return arr;
     }
+    //* Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
     public static void searchMinMax(){
         int[] arr = {1, 5, 3, 2, 11, -4, 5, 2, 4, 8, 9, 1};
         int min = arr[0];
