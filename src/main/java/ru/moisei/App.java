fc9aa21fa9e9066a6acd4e604e2512216df07dbe
Andrei M <moisei555@yahoo.com>
2024-08-02 10:06:57 +0500

    git init

--- src/main/java/ru/moisei/App.java
+++ src/main/java/ru/moisei/App.java
@@ -91,7 +91,7 @@ public class App {
         int[] arr = {1, 5, 3, 2, 11, -4, 5, 2, 4, 8, 9, 1};
         int min = arr[0];
         int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
